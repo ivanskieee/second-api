976575200652556baeafc6523401b52fa364a5f7
Ivan Brilata <brilataivan86@gmail.com>
2025-01-21 22:24:45 +0800

    di ko maconnect bukas ulit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,11 @@
 class UsersController < ApplicationController
   def create
-
+    render json: { message: "User created successfully" }
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:email, :password)
+    params.require(:user).permit(:email, :password, :first_name, :last_name)
   end
 end
